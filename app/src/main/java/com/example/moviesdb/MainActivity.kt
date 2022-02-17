14444398ff08144394f4e0e094f2ebeaa7f14444
youssef7agar <youssef7agar24@gmail.com>
2022-02-17 20:02:07 +0200

    Initial commit

---
+++ app/src/main/java/com/example/moviesdb/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.moviesdb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
